0d9a379bd3991d034cbe8b62fd6c6616430218a9
Vitor Lobo Ramos <lobocode@gmail.com>
2025-03-13 14:14:16 -0300

    Add global using directives and Windows platform attribute

---
+++ GlobalUsings.cs
@@ -0,0 +1,11 @@
+global using System;
+global using System.IO;
+global using System.Management;
+global using System.Diagnostics;
+global using System.Threading;
+global using Microsoft.Win32;
+global using System.Collections.Generic;
+global using System.Security.Principal;
+global using System.Runtime.Versioning;
+
+[assembly: SupportedOSPlatform("windows")] 
